bd0273d6f74cae31cd35b7ff73306c4c6c1dadb0
Heitor Buzzatto <hdb_523@hotmail.com>
2022-02-06 15:55:29 -0300

    Little improvement in board printing

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -13,6 +13,14 @@ public class ChessPiece extends Piece {
 		this.color = color;
 	}
 
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		// TODO Auto-generated method stub
